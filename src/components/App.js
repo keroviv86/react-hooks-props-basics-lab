67b0ac43bf38704ab80b4e52956e6036d4fc1ca0
Vivian Yim <keroviv86@gmail.com>
2022-01-10 20:00:52 -0600

    commit tree 480ee7201e44f2a9928b679d15a4140f594869d2

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,28 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import Links from "./Links";
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        color= {user.color}
+        username = {user.name}
+        city = {user.city}
+      />
+      <About bio = {user.bio} >
+        <Links
+          github = {user.links.github}
+          linkedin = {user.links.linkedin}
+        />
+
+      </About>
     </div>
   );
 }
